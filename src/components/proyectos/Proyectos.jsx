916c04448b4f9ded211402f9c9241fa5910f2614
Benji Martinez Flores <49083076+Benji-Mtz@users.noreply.github.com>
2021-06-14 17:29:00 -0500

    estamos en el context api

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Sidebar from '../layout/Sidebar'
+import Barra from '../layout/Barra'
+import FormTarea from '../tareas/FormTarea'
+import ListadoTareas from '../tareas/ListadoTareas'
 
 const Proyectos = () => {
     return (
@@ -8,9 +11,11 @@ const Proyectos = () => {
 
 
             <div className="seccion-principal">
+                <Barra />
                 <main>
+                    <FormTarea />
                     <div className="contenedor-tareas">
-
+                        <ListadoTareas />
                     </div>
                 </main>
             </div>
